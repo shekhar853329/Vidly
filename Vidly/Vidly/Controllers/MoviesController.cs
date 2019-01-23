3466a7caa2204f2b7946aa8da20904ae601fe794
Shekhar <shekhar853329@gmail.com>
2019-01-24 03:04:45 +0530

    Random Controller added - Getting Started

---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var Movie = new Movie()
+            {
+                Name = "Shrek"
+            };
+            return View(Movie);
+        }
+    }
+}
