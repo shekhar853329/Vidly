001a13c3a150082f8d000aae79043d51827685f6
Shekhar <shekhar853329@gmail.com>
2019-01-24 22:40:08 +0530

    Action Parameters

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -5,14 +5,37 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        [HttpGet]
         public ActionResult Random()
         {
             var Movie = new Movie()
             {
                 Name = "Shrek"
             };
+            //return View(Movie);
+            //return Content("hello world");
+            //return HttpNotFound();
             return View(Movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id= " + id.ToString());
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (string.IsNullOrWhiteSpace((sortBy)))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(string.Format("PageIndex={0},sortBy={1}", pageIndex, sortBy));
         }
     }
 }
